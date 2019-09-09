bc55af84381c644d726ba0fae92cebc67099286e
Alain Hélaïli <helaili@github.com>
2019-09-09 17:33:20 +0200

    What about actually commiting the script?

---
+++ .github/scripts/udpate-deployment-status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+DEPLOYMENT_STATUS_URL=$(jq -r .deployment.statuses_url $GITHUB_EVENT_PATH)
+DEPLOYMENT_ENVIRONMENT=$(jq -r .deployment.environment $GITHUB_EVENT_PATH)
+
+DESCRIPTION="Deployed in $DEPLOYMENT_ENVIRONMENT"
+TARGET_URL="http://githubookstore.azurewebsites.net/"
+
+JSON_STRING=$( jq -n \
+                  --arg desc "$DESCRIPTION" \
+                  --arg url "$TARGET_URL" \
+                  '{"state": "success", "description": $desc, "environment_url": $url}' )
+
+curl -k -H "Authorization: token $GITHUB_TOKEN" -H "accept: application/vnd.github.ant-man-preview+json,application/vnd.github.flash-preview+json" -d "$JSON_STRING" $DEPLOYMENT_STATUS_URL
