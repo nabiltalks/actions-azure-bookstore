5f299d9f952cf3cedfa372f224c54a13662d8586
Alain Hélaïli <helaili@github.com>
2019-10-10 13:34:24 +0200

    Need to use the head ref in a PR event

--- .github/scripts/delete-deployments.sh
+++ .github/scripts/delete-deployments.sh
@@ -1,3 +1,3 @@
-curl -v -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${GITHUB_REPOSITORY}/deployments?ref=$(echo $GITHUB_REF | cut -f3 -d "/") > deployments.json
+curl -v -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/${GITHUB_REPOSITORY}/deployments?ref=$(echo $GITHUB_HEAD_REF | cut -f3 -d "/") > deployments.json
 
-jq -c 'map(select(.environment | contains ("test")))' deployments.json > test-deployments.json
+jq -r 'map(select(.environment | contains ("test"))) | .[].statuses_url' deployments.json | xargs curl -v -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.ant-man-preview+json" -d '{"state": "inactive"}'
